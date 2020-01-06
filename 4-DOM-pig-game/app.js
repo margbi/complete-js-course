16f7dea30325f78fb7ce5bd8f59d9193a78ef664
Martyna G <martynagbi@gmail.com>
2020-01-06 14:25:34 +0100

    code cleanup

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -1,8 +1,8 @@
 /*
 GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he wishes. Each result gets added to his ROUND score
+- There are 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wants. Each result gets added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
@@ -16,15 +16,15 @@ init();
 // Rolling the dice event
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
-        // 1. Random number generation
+        // Random number generation
         var dice = Math.floor(Math.random() * 6) + 1;
         
-        // 2. Display the result
+        // Displaying the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
         
-        // 3. Update round score if the rolled number was not a 1
+        // Updating round score if the rolled number was not a 1
         if (dice !== 1) {
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
@@ -40,10 +40,10 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
         // Adding points to global score
         scores[activePlayer] += roundScore;
-        // Update the UI
+        // Updating UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-        // Check whether the player won the game
-        if (scores[activePlayer] >= 20) {
+        // Checking whether the player won the game
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -72,6 +72,7 @@ function nextPlayer() {
 document.querySelector('.btn-new').addEventListener('click', init);
 
 function init() {
+    // Setting start values
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
@@ -79,6 +80,7 @@ function init() {
     
     // Removing the dice image
     document.querySelector('.dice').style.display = 'none';
+    
     // Setting all values to 0
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
